1361b8ed8f0bedcbfe023cc3d77c16075fbe78f9
DaoLuoLTS <61882306+ShenHaiSu@users.noreply.github.com>
2025-04-27 13:21:21 +0800

    Update HiFiNi-AutoReply.build.user.js

--- HiFiNi-AutoReply.build.user.js
+++ HiFiNi-AutoReply.build.user.js
@@ -7,6 +7,7 @@
 // @match        https://hifini.com/thread-256032.htm
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=hifini.com
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 (function () {
